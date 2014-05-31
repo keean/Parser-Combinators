7c29653ac93af48913df5cf4f0f6b8bc872e52d8
keean <keean@fry-it.com>
2014-05-31 20:46:06 +0100

    add debugging names for parsers, better location tracking and better diagnostic errors.

--- test_combinators.cpp
+++ test_combinators.cpp
@@ -1,6 +1,8 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <sstream>
+
 #include "templateio.hpp"
 #include "parser_combinators.hpp"
 #include "profile.hpp"
@@ -12,22 +14,21 @@ using namespace std;
 
 struct parse_int {
     parse_int() {}
-    void operator() (vector<int> *ts, string const& num, string const& sep) const {
+    void operator() (vector<int> *ts, string const& num) const {
         ts->push_back(stoi(num));
     }
 } const parse_int;
 
 struct parse_line {
     parse_line() {}
-    void operator() (vector<vector<int>> *ts, vector<int> &line, string const& line_sep) const {
+    void operator() (vector<vector<int>> *ts, vector<int> &line) const {
         ts->push_back(move(line)); // move modifies 'line' so don't make it const
     }
 } const parse_line;
 
-auto const recognise_number = some(accept(is_digit));
-auto const recognise_space = some(accept(is_space));
-auto const recognise_separator = option(accept(is_char(',')) && discard(option(recognise_space)));
-auto const parse_csv = some(all(parse_line, some(all(parse_int, recognise_number, recognise_separator)), option(recognise_space)));
+auto const number_tok = tokenise(some(accept(is_digit)));
+auto const separator_tok = tokenise(accept(is_char(',')));
+auto const parse_csv = some(all(parse_line, some(all(parse_int, number_tok) && discard(separator_tok))));
 
 class csv_parser {
     pstream in;
@@ -39,9 +40,13 @@ public:
         decltype(parse_csv)::result_type a; 
 
         bool b;
-        {
+        try {
             profile<csv_parser> p;
             b = parse_csv(in, &a);
+        } catch (parse_error& e) {
+            stringstream err;
+            err << e;
+            throw runtime_error(err.str());
         }
 
         if (b) {
@@ -50,9 +55,9 @@ public:
             cout << "FAIL\n";
         }
 
-        // cout << a << "\n";
+        //cout << a << "\n";
         
-        return in.get_count();
+        return in.get_pos();
     }
 };
 
@@ -63,29 +68,15 @@ int main(int const argc, char const *argv[]) {
         cerr << "no input files\n";
     } else {
         for (int i = 1; i < argc; ++i) {
-            try {
-                fstream in(argv[i], ios_base::in);
-                cout << argv[i] << "\n";
+            fstream in(argv[i], ios_base::in);
+            cout << argv[i] << "\n";
 
-                if (in.is_open()) {
-                    csv_parser csv(in);
-                    profile<csv_parser>::reset();
-                    int const chars_read = csv();
-                    double const mb_per_s = static_cast<double>(chars_read) / static_cast<double>(profile<csv_parser>::report());
-                    cout << "parsed: " << mb_per_s << "MB/s\n";
-                }
-            } catch (parse_error& e) {
-                cerr << argv[i] << ": " << e.what()
-                    << " " << e.exp
-                    << " found ";
-                if (is_print(e.sym)) {
-                    cerr << "'" << static_cast<char>(e.sym) << "'";
-                } else {
-                    cerr << "0x" << hex << e.sym;
-                }
-                cerr << " at line " << e.row
-                    << ", column " << e.col << "\n";
-                return 2;
+            if (in.is_open()) {
+                csv_parser csv(in);
+                profile<csv_parser>::reset();
+                int const chars_read = csv();
+                double const mb_per_s = static_cast<double>(chars_read) / static_cast<double>(profile<csv_parser>::report());
+                cout << "parsed: " << mb_per_s << "MB/s\n";
             }
         }
     }
