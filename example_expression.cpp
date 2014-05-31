f64c7975eb6cd53c410f9b4d4dc2ed853bf4ff79
keean <keean@fry-it.com>
2014-05-31 20:46:41 +0100

    add debugging names for parsers, better location tracking and better diagnostic errors.

--- example_expression.cpp
+++ example_expression.cpp
@@ -1,6 +1,8 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <sstream>
+
 #include "templateio.hpp"
 #include "parser_combinators.hpp"
 #include "profile.hpp"
@@ -48,26 +50,26 @@ struct return_div {
 auto const number_tok = tokenise(some(accept(is_digit)));
 auto const start_tok = tokenise(accept(is_char('(')));
 auto const end_tok = tokenise(accept(is_char(')')));
-auto const add_tok = tokenise( accept(is_char('+')));
-auto const sub_tok = tokenise( accept(is_char('-')));
-auto const mul_tok = tokenise( accept(is_char('*')));
-auto const div_tok = tokenise( accept(is_char('/')));
+auto const add_tok = tokenise(accept(is_char('+')));
+auto const sub_tok = tokenise(accept(is_char('-')));
+auto const mul_tok = tokenise(accept(is_char('*')));
+auto const div_tok = tokenise(accept(is_char('/')));
 
 parser_handle<int> const additive_expr(parser_handle<int> const e) {
-    return log("+", all(return_add, e, add_tok, e))
-        || log("-", all(return_sub, e, sub_tok, e));
+    return log("+", attempt(all(return_add, e, add_tok, e)))
+        || log("-", attempt(all(return_sub, e, sub_tok, e)));
 }
 
 parser_handle<int> const multiplicative_expr(parser_handle<int> const e) {
-    return log("*", all(return_mul, e, mul_tok, e))
-        || log("/", all(return_div, e, div_tok, e));
+    return log("*", attempt(all(return_mul, e, mul_tok, e)))
+        || log("/", attempt(all(return_div, e, div_tok, e)));
 }
 
-parser_handle<int> const expression = (
+parser_handle<int> const expression = strict(attempt(
         discard(start_tok)
-        && (additive_expr(reference(expression)) || multiplicative_expr(reference(expression)))
+        && (attempt(additive_expr(reference(expression))) || multiplicative_expr(reference(expression)))
         && discard(end_tok))
-    || all(return_int, number_tok);
+    || all(return_int, number_tok));
 
 class csv_parser {
     pstream in;
@@ -81,9 +83,13 @@ public:
         decltype(parser)::result_type a {}; 
 
         bool b;
-        {
+        try {
             profile<csv_parser> p;
             b = parser(in, &a);
+        } catch (parse_error& e) {
+            stringstream err;
+            err << e;
+            throw runtime_error(err.str());
         }
 
         if (b) {
@@ -94,7 +100,7 @@ public:
 
         cout << a << "\n";
         
-        return in.get_count();
+        return in.get_pos();
     }
 };
 
@@ -105,29 +111,15 @@ int main(int const argc, char const *argv[]) {
         cerr << "no input files\n";
     } else {
         for (int i = 1; i < argc; ++i) {
-            try {
-                fstream in(argv[i], ios_base::in);
-                cout << argv[i] << "\n";
+            fstream in(argv[i], ios_base::in);
+            cout << argv[i] << "\n";
 
-                if (in.is_open()) {
-                    csv_parser csv(in);
-                    profile<csv_parser>::reset();
-                    int const chars_read = csv();
-                    double const mb_per_s = static_cast<double>(chars_read) / static_cast<double>(profile<csv_parser>::report());
-                    cout << "parsed: " << mb_per_s << "MB/s\n";
-                }
-            } catch (parse_error& e) {
-                cerr << argv[i] << ": " << e.what()
-                    << " " << e.exp
-                    << " found ";
-                if (is_print(e.sym)) {
-                    cerr << "'" << static_cast<char>(e.sym) << "'";
-                } else {
-                    cerr << "0x" << hex << e.sym;
-                }
-                cerr << " at line " << e.row
-                    << ", column " << e.col << "\n";
-                return 2;
+            if (in.is_open()) {
+                csv_parser csv(in);
+                profile<csv_parser>::reset();
+                int const chars_read = csv();
+                double const mb_per_s = static_cast<double>(chars_read) / static_cast<double>(profile<csv_parser>::report());
+                cout << "parsed: " << mb_per_s << "MB/s\n";
             }
         }
     }
