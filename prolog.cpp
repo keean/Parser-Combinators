60de5c79fe61eebf42c17fcf048bed63e088fdfe
keean <keean@fry-it.com>
2015-05-20 19:17:09 +0100

    memory for abstract syntax now owned by program object

--- prolog.cpp
+++ prolog.cpp
@@ -8,11 +8,11 @@ template <typename Range>
 int parse(Range const &r, program& prog) {
     decltype(parser)::result_type a {}; 
     typename Range::iterator i = r.first;
-    inherited_attributes st(prog.names);
+    inherited_attributes st(prog);
 
     profile<expression_parser> p;
 
-    if (parser(i, r, &prog.db, &st)) {
+    if (parser(i, r, &prog, &st)) {
         cout << "OK" << endl;
     } else {
         cout << "FAIL" << endl;
@@ -36,7 +36,7 @@ int main(int const argc, char const *argv[]) {
             cout << argv[i] << endl;
             program prog;
             int const chars_read = parse(in, prog);
-            cout << prog << endl;
+            cout << prog;
             double const mb_per_s = static_cast<double>(chars_read) / static_cast<double>(profile<expression_parser>::report());
             cout << "parsed: " << mb_per_s << "MB/s" << endl;
         }
--- prolog.hpp
+++ prolog.hpp
@@ -25,77 +25,103 @@ using namespace std;
 // supertype. Clauses combine heads and goals, and keep track of repeated 
 // variables in the head for efficient post-unification cycle checking.
 
-using name_t = std::set<std::string>::const_iterator;
+using atom_t = std::set<std::string>::const_iterator;
 
-struct name_less {
-    bool operator() (name_t const& x, name_t const& y) const {
+struct atom_less {
+    bool operator() (atom_t const& x, atom_t const& y) const {
         return &(*x) < &(*y);
     }
 };
 
-struct type_expression {
-    virtual void accept(class expr_visitor* v) = 0;
+struct managed {
+    virtual ~managed() {}
 };
 
-struct type_variable : public type_expression {
-    name_t const name;
-    type_variable(name_t n) : name(n) {}
-    virtual void accept(class expr_visitor* v) override;
+struct term : public managed {
+    virtual void accept(class term_visitor* v) = 0;
+    virtual ~term() {}
 };
 
-struct type_struct : public type_expression {
-    name_t const name;
-    vector<type_expression*> const args;
-    template <typename As> type_struct(name_t n, As&& as) : name(n), args(forward<As>(as)) {}
-    virtual void accept(class expr_visitor* v) override;
+struct variable : public term {
+    atom_t const atom;
+    virtual void accept(class term_visitor* v) override;
+private:
+    friend class program;
+    variable(atom_t n) : atom(n) {}
 };
 
-struct expr_visitor {
-    virtual void visit(type_variable *t) = 0;
-    virtual void visit(type_struct *t) = 0;
+struct compound : public term {
+    atom_t const atom;
+    vector<term*> const args;
+    virtual void accept(class term_visitor* v) override;
+private:
+    friend class program;
+    template <typename As> compound(atom_t n, As&& as) : atom(n), args(forward<As>(as)) {}
 };
 
-void type_variable::accept(expr_visitor* v) {v->visit(this);}
-void type_struct::accept(expr_visitor* v) {v->visit(this);}
+struct term_visitor {
+    virtual void visit(variable *t) = 0;
+    virtual void visit(compound *t) = 0;
+};
 
+void variable::accept(term_visitor* v) {v->visit(this);}
+void compound::accept(term_visitor* v) {v->visit(this);}
 
+struct clause : public managed {
+    compound* const head;
+    vector<compound*> const impl;
+    set<variable*> const reps;
 
-struct clause {
-    type_struct* const head;
-    vector<type_struct*> const impl;
-    set<type_variable*> const reps;
-
+private:
+    friend class program;
     template <typename Is, typename Rs>
-    clause(type_struct* h, Is&& is, Rs&& rs) : head(h), impl(forward<Is>(is)),
+    clause(compound* h, Is&& is, Rs&& rs) : head(h), impl(forward<Is>(is)),
         reps(forward<Rs>(rs)) {}
 };
 
-
-
-using db_t = multimap<name_t, struct clause*, name_less>; 
-
 struct program {
-    set<string> names;
-    db_t db;
+    vector<unique_ptr<managed>> region;
+    set<string> atoms;
+    multimap<atom_t, struct clause*, atom_less> db;
+    vector<clause*> goals;
+
+    variable* new_variable(atom_t n) {
+        variable* v = new variable(n);
+        region.emplace_back(v);
+        return v;
+    }
+
+    template <typename As> compound* new_compound(atom_t n, As&& as) {
+        compound* c = new compound(n, forward<As>(as));
+        region.emplace_back(c);
+        return c;
+    }
+
+    template <typename Is, typename Rs>
+    clause* new_clause(compound* h, Is&& is, Rs&& rs) {
+        clause* c = new clause(h, forward<Is>(is), forward<Rs>(rs));
+        region.emplace_back(c);
+        return c;
+    }
 };
 
 //----------------------------------------------------------------------------
 // Show Abstract Syntax
 
-class expr_show : public expr_visitor {
+class expr_show : public term_visitor {
     ostream& out;
 
-    virtual void visit(type_variable* t) override {
-        out << *(t->name);
+    virtual void visit(variable* t) override {
+        out << *(t->atom);
     }
 
-    virtual void visit(type_struct* t) override {
-        if (::ispunct((*(t->name))[0]) && t->args.size() == 2) {
+    virtual void visit(compound* t) override {
+        if (::ispunct((*(t->atom))[0]) && t->args.size() == 2) {
             t->args[0]->accept(this);
-            cout << " " << *(t->name) << " ";
+            cout << " " << *(t->atom) << " ";
             t->args[1]->accept(this);
         } else {
-            out << *(t->name);
+            out << *(t->atom);
             if (t->args.size() > 0) {
                 out << "(";
                 for (auto i = t->args.cbegin(); i != t->args.cend(); ++i) {
@@ -113,12 +139,12 @@ class expr_show : public expr_visitor {
 public:
     expr_show(ostream& out) : out(out) {}
 
-    void operator() (type_expression* t) {
+    void operator() (term* t) {
         t->accept(this);
     }
 };
 
-ostream& operator<< (ostream& out, type_expression* exp) {
+ostream& operator<< (ostream& out, term* exp) {
     expr_show eshow(out);
     eshow(exp);
     return out;
@@ -167,6 +193,10 @@ ostream& operator<< (ostream& out, program const& p) {
         string pad(" ", tab - to_string(i).size());
         out << pad << i << ". " << j->second;
     }
+    out << endl;
+    for (auto j = p.goals.cbegin(); j != p.goals.cend(); ++j) {
+        out << *j << endl;
+    }
     return out;
 };
 
@@ -179,28 +209,28 @@ ostream& operator<< (ostream& out, program const& p) {
 // performance) or a safety measure to make sure we deal with copying if we 
 // want backtracking.
 
-using var_t = map<name_t, type_variable*>::const_iterator;
+using var_t = map<atom_t, variable*>::const_iterator;
 
 struct inherited_attributes {
-    set<string>& names; // global name table
-    map<name_t, type_variable*, name_less> variables; 
-    set<type_variable*> repeated;
-    set<type_variable*> repeated_in_goal;
+    program& prog;
 
-    //inherited_attributes() {};
+    map<atom_t, variable*, atom_less> variables; 
+    set<variable*> repeated;
+    set<variable*> repeated_in_goal;
+
     inherited_attributes(inherited_attributes const&) = delete;
     inherited_attributes& operator= (inherited_attributes const&) = delete;
 
-    name_t get_name(string const& name) {
-        name_t const n = names.find(name);
-        if (n == names.end()) {
-            return names.insert(name).first;
+    atom_t get_atom(string const& atom) {
+        atom_t const n = prog.atoms.find(atom);
+        if (n == prog.atoms.end()) {
+            return prog.atoms.insert(atom).first;
         } else {
             return n;
         }
     }
 
-    inherited_attributes(set<string>& ns) : names(ns) {}
+    inherited_attributes(program& p) : prog(p) {}
 };
 
 //----------------------------------------------------------------------------
@@ -212,11 +242,11 @@ struct inherited_attributes {
 
 struct return_variable {
     return_variable() {}
-    void operator() (type_variable** res, string const& name, inherited_attributes* st) const {
-        name_t const n = st->get_name(name);
+    void operator() (variable** res, string const& atom, inherited_attributes* st) const {
+        atom_t const n = st->get_atom(atom);
         var_t i = st->variables.find(n);
         if (i == st->variables.end()) {
-            type_variable* const var = new type_variable(n);
+            variable* const var = st->prog.new_variable(n);
             st->variables.insert(make_pair(n, var));
             *res = var;
         } else {
@@ -228,21 +258,21 @@ struct return_variable {
 
 struct return_args {
     return_args() {}
-    void operator() (vector<type_expression*>* res, type_expression* t1, inherited_attributes*) const {
+    void operator() (vector<term*>* res, term* t1, inherited_attributes*) const {
         res->push_back(t1);
     }
 } const return_args;
 
 struct return_struct {
     return_struct() {}
-    void operator() (type_struct** res, string const& name, vector<type_expression*>& args, inherited_attributes* st) const {
-        *res = new type_struct(st->get_name(name), args);
+    void operator() (compound** res, string const& atom, vector<term*>& args, inherited_attributes* st) const {
+        *res = st->prog.new_compound(st->get_atom(atom), args);
     }
 } const return_struct;
 
 struct return_term {
     return_term() {}
-    void operator() (type_expression** res, int n, type_variable* v, type_struct *s, inherited_attributes* st) const {
+    void operator() (term** res, int n, variable* v, compound *s, inherited_attributes* st) const {
         switch (n) {
             case 0:
                 *res = v;
@@ -256,11 +286,11 @@ struct return_term {
 
 struct return_op_exp_exp {
     return_op_exp_exp() {}
-    void operator() (type_expression** res, type_expression* t1, pair<string, type_expression*> const& t2, inherited_attributes* st) const {
+    void operator() (term** res, term* t1, pair<string, term*> const& t2, inherited_attributes* st) const {
         if (!t2.first.empty()) {
-            name_t o = st->get_name(t2.first);
-            vector<type_expression*> a {t1, t2.second};
-            *res = new type_struct(o, a);
+            atom_t o = st->get_atom(t2.first);
+            vector<term*> a {t1, t2.second};
+            *res = st->prog.new_compound(o, a);
         } else {
             *res = t1;
         }
@@ -269,19 +299,19 @@ struct return_op_exp_exp {
 
 struct return_op_var_exp {
     return_op_var_exp() {}
-    void operator() (type_struct** res, type_variable* t1, string const& oper, type_expression* t2, inherited_attributes* st) const {
-        name_t o = st->get_name(oper);
-        vector<type_expression*> a {t1, t2};
-        *res = new type_struct(o, a);
+    void operator() (compound** res, variable* t1, string const& oper, term* t2, inherited_attributes* st) const {
+        atom_t o = st->get_atom(oper);
+        vector<term*> a {t1, t2};
+        *res = st->prog.new_compound(o, a);
     }
 } const return_op_var_exp;
 
 struct return_oper_term {
     return_oper_term() {}
     void operator() (
-        pair<string, type_expression*>* res,
+        pair<string, term*>* res,
         string const& oper,
-        type_expression* term,
+        term* term,
         inherited_attributes* st
     ) const {
         *res = make_pair(oper, term);
@@ -290,11 +320,11 @@ struct return_oper_term {
 
 struct return_op_stc_exp {
     return_op_stc_exp() {}
-    void operator() (type_struct** res, type_struct* t1, pair<string, type_expression*> const& t2, inherited_attributes* st) const {
+    void operator() (compound** res, compound* t1, pair<string, term*> const& t2, inherited_attributes* st) const {
         if (!t2.first.empty()) {
-            name_t o = st->get_name(t2.first);
-            vector<type_expression*> a {t1, t2.second};
-            *res = new type_struct(o, a);
+            atom_t o = st->get_atom(t2.first);
+            vector<term*> a {t1, t2.second};
+            *res = st->prog.new_compound(o, a);
         } else {
             *res = t1;
         }
@@ -303,7 +333,7 @@ struct return_op_stc_exp {
  
 struct return_head {
     return_head() {}
-    void operator() (type_struct** res, type_struct* str, inherited_attributes* st) const {
+    void operator() (compound** res, compound* str, inherited_attributes* st) const {
         *res = str;
         st->repeated_in_goal = st->repeated;
     }
@@ -311,15 +341,20 @@ struct return_head {
 
 struct return_goal {
     return_goal() {}
-    void operator() (vector<type_struct*>* res, type_struct* impl, inherited_attributes*) const {
+    void operator() (vector<compound*>* res, compound* impl, inherited_attributes*) const {
         res->push_back(impl);
     }
 } const return_goal;
 
 struct return_clause {
     return_clause() {}
-    void operator() (db_t* res, type_struct* head, vector<type_struct*>& impl, inherited_attributes* st) const {
-        res->emplace(head->name, new clause(head, impl, st->repeated_in_goal));
+    void operator() (
+        void* res,
+        compound* head,
+        vector<compound*>& impl,
+        inherited_attributes* st
+    ) const {
+        st->prog.db.emplace(head->atom, st->prog.new_clause(head, impl, st->repeated_in_goal));
         st->variables.clear();
         st->repeated.clear();
         st->repeated_in_goal.clear();
@@ -328,18 +363,15 @@ struct return_clause {
 
 struct return_goals {
     return_goals() {}
-    void operator() (db_t* res, vector<type_struct*>& impl, inherited_attributes* st) const {
-        vector<type_expression*> vars;
+    void operator() (program* res, vector<compound*>& impl, inherited_attributes* st) const {
+        vector<term*> vars;
         for (auto v : st->variables) {
             vars.push_back(v.second);
         }
 
-        name_t n = st->get_name("goal");
-
-        res->emplace(make_pair(n, new clause(
-            new type_struct(n, vars), impl, set<type_variable*> {}
-        )));
-
+        atom_t n = st->get_atom("goal");
+        st->prog.goals.emplace_back(st->prog.new_clause(
+            st->prog.new_compound(n, vars), impl, set<variable*> {}));
         st->variables.clear();
         st->repeated.clear();
         st->repeated_in_goal.clear();
@@ -375,7 +407,7 @@ template <typename T> using pshand = pstream_handle<T, inherited_attributes>;
 // higher order parsers.
 
 // parse atom and list of arguments.
-pshand<type_struct*> recursive_struct(pshand<type_expression*> const t) {
+pshand<compound*> recursive_struct(pshand<term*> const t) {
     return define("struct", all(return_struct,
         atom,
         option(discard(open_tok) && sep_by(all(return_args, t), sep_tok) && discard(close_tok))
@@ -383,7 +415,7 @@ pshand<type_struct*> recursive_struct(pshand<type_expression*> const t) {
 }
 
 // parse a term that is either a variable or an atom/struct
-pshand<type_expression*> recursive_term(pshand<type_expression*> const t) {
+pshand<term*> recursive_term(pshand<term*> const t) {
     return define("term", any(return_term,
         variable,
         recursive_struct(t))
@@ -391,7 +423,7 @@ pshand<type_expression*> recursive_term(pshand<type_expression*> const t) {
 }
 
 // parse a term, followed by optional operator and term.
-pshand<type_expression*> recursive_oper(pshand<type_expression*> const t) {
+pshand<term*> recursive_oper(pshand<term*> const t) {
     return all(return_op_exp_exp,
         recursive_term(t),
         option(all(return_oper_term, attempt(oper), t))
